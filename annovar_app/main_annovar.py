b50fe4eb64b0d0d081c27b2ac02f95750ac38352
Laura Vail <lvail1@jhu.edu>
2023-10-19 14:37:57 -0400

    Revisions to the annovar app and user guide

--- annovar_app/main_annovar.py
+++ annovar_app/main_annovar.py
@@ -4,28 +4,20 @@ import os
 import shutil
 import subprocess
 import sys
-import tarfile
 from datetime import datetime
 from pathlib import Path
 from pytz import timezone
 from subprocess import PIPE
-from utils import AnnovarException
+from utils import run_command, AnnovarException
 
 
-def main(vcf_files, version, ref_data_tar):
-    debug_log('in main()')
-    global annovar_dir  # directory with Annovar's perl scripts
-    annovar_dir = '../annovar_install/annovar/'  # local testing path
-    # annovar_dir = '/usr/src/phenodb_annovar/annovar/'  # pathway in container
-
-    # extract annovar reference data from tar
+def main(vcf_files, version_name, ref_data_tar):
+    # no second arg or True for container; False for local test run
+    annovar_dir_path = get_paths()
     ref_data_dir = extract(ref_data_tar)
+    print_dir_contents([annovar_dir_path, ref_data_dir])
 
-    # TEST: print and write to debug all recursive files in working dir
-    debug_log('All contents of working dir:')
-    for path in Path('.').rglob('*'):
-        debug_log(path)
-
+    # convert then annotate each VCF
     for vcf in vcf_files:
         debug_log('in loop for vcf: ' + vcf)
         if vcf.endswith('.gz'):
@@ -33,27 +25,40 @@ def main(vcf_files, version, ref_data_tar):
         
         # convert vcf to Annovar format, and write to a new file with suffix 'avinput'
         vcf_converted = os.path.basename(vcf[:-3] + 'avinput')
-        write_conversion(vcf, vcf_converted)
+        write_conversion(vcf, vcf_converted, annovar_dir_path)
         
         # annotate the converted file with Annovar reference data
         try:
-            write_annotation(vcf_converted, version, ref_data_dir)
+            write_annotation(vcf_converted, version_name, annovar_dir_path, ref_data_dir)
         except Exception as ex:
             raise AnnovarException('Converted VCF could not be annotated:' + vcf_converted, ex)
-    
 
-# write reference data dir to working directory
-def extract(ref_data_tar):
+
+def get_paths(container=True):
+    if container:  
+        prefix = '/usr/src/phenodb_annovar/'  # production paths in container
+    else:  
+        prefix = './'  # paths to run as a script on local machine
+    return prefix + 'annovar/'
+
+
+def print_dir_contents(directories):
+    for direct in directories:
+        debug_log('Contents of directory: ' + direct)
+        for path in Path(direct).rglob('*'):
+            debug_log('    ' + str(path))
+
+    
+def extract(ref_data_filename):
     debug_log('in extract()')
-    try:
-        with tarfile.open(ref_data_tar, 'r:gz') as in_tar:
-            ref_data = os.path.basename(ref_data_tar[:-7])  # remove 'tar.gz' to get dir name
-            if not os.path.exists(ref_data):
-                os.makedirs(ref_data)
-            in_tar.extractall()  # no arg default: extract to working dir, name is tar without ext
-        return ref_data
-    except Exception as ex:
-        raise AnnovarException('File could not be extracted:' + ref_data_tar, ex)
+    cmd_extract = ['tar -xvf ' + ref_data_filename]
+    debug_log('cmd_extract: ' + ' '.join(cmd_extract))
+    result = subprocess.run(cmd_extract, shell=True, stderr=PIPE)
+    if result.returncode != 0:
+        str_err = result.stderr.decode('utf-8')
+        raise AnnovarException('Reference data could not be extracted from ' + ref_data_filename + ', error_string: ' + str_err, '')
+    else:
+        return os.path.basename(ref_data_filename[:-7])  # dir is path without '.tar.gz' from end
 
 
 def uncompress(file_in):
@@ -69,9 +74,9 @@ def uncompress(file_in):
 
 
 # writes converted file to working directory
-def write_conversion(vcf, vcf_converted):
-    debug_log('in write_conversion()')
-    cmd_convert = ['perl', annovar_dir + 'convert2annovar.pl', '--format', 'vcf4', '--includeinfo', '--withzyg', '--outfile', vcf_converted, vcf]
+def write_conversion(vcf, vcf_converted, annovar_dir_path):
+    cmd_convert = ['perl', annovar_dir_path + 'convert2annovar.pl', '--format', 'vcf4', '--includeinfo', '--withzyg', '--outfile', vcf_converted, vcf]
+    debug_log('cmd_convert: ' + ' '.join(cmd_convert))
     result = subprocess.run(cmd_convert, stderr=PIPE)
     if result.returncode != 0:
         str_err = result.stderr.decode('utf-8')
@@ -79,17 +84,15 @@ def write_conversion(vcf, vcf_converted):
 
 
 # writes output to working directory
-# suffix automatically added by annovar is 'hg19_multianno.txt'
-# smaller values for testing:
+# suffix automatically added by annovar is eg 'hg38_multianno.txt'
+# smaller values for faster test runs:
     # data_sources = 'refGene,ensGene'
     # ops = 'g,g'
-def write_annotation(vcf_converted, version_name, ref_data_dir):
+def write_annotation(vcf_converted, version_name, annovar_dir_path, ref_data_dir):
     debug_log('in write_annotation()')
     out_name = vcf_converted[:-8]  # remove '.avinput' file extension
-    # data_sources = 'refGene,ensGene,esp6500siv2_all,esp6500siv2_aa,esp6500siv2_ea,1000g2014oct_all,1000g2014oct_afr,1000g2014oct_amr,1000g2014oct_eas,1000g2014oct_eur,1000g2014oct_sas,1000g2015aug_all,1000g2015aug_afr,1000g2015aug_amr,1000g2015aug_eas,1000g2015aug_eur,1000g2015aug_sas,avsnp142,avsnp144,avsnp147,avsnp150,exac03'
-    # ops = 'g,g,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f'
-    data_sources = 'refGene,ensGene'
-    ops = 'g,g'
+    data_sources = 'refGene,ensGene,esp6500siv2_all,esp6500siv2_aa,esp6500siv2_ea,1000g2014oct_all,1000g2014oct_afr,1000g2014oct_amr,1000g2014oct_eas,1000g2014oct_eur,1000g2014oct_sas,1000g2015aug_all,1000g2015aug_afr,1000g2015aug_amr,1000g2015aug_eas,1000g2015aug_eur,1000g2015aug_sas,avsnp142,avsnp144,avsnp147,avsnp150,exac03'
+    ops = 'g,g,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f'
 
     if version_name == 'Hg19':
         version = 'hg19'
@@ -98,7 +101,8 @@ def write_annotation(vcf_converted, version_name, ref_data_dir):
     else:
         raise AnnovarException('Invalid human assembly version: ' + version_name, '')
 
-    cmd_annotate = ['perl', annovar_dir + 'table_annovar.pl', '-buildver', version, '-otherinfo', '-remove', '-protocol', data_sources, '-operation', ops, '--outfile', out_name, vcf_converted, ref_data_dir]
+    cmd_annotate = ['perl', annovar_dir_path + 'table_annovar.pl', '-buildver', version, '-otherinfo', '-remove', '-protocol', data_sources, '-operation', ops, '--outfile', out_name, vcf_converted, ref_data_dir]
+    debug_log('cmd_annotate: ' + ' '.join(cmd_annotate))
     result = subprocess.run(cmd_annotate, stderr=PIPE)
     if result.returncode != 0:
         str_err = result.stderr.decode('utf-8')
@@ -126,7 +130,7 @@ def parse_args(argv):
 if __name__ == '__main__':
     result = parse_args(sys.argv[1:])
     debug_log('sys.argv:' + str(sys.argv), init=True)
-    debug_log(str(result))
+    debug_log('\n' + str(result))
     ref_data_tar = result.refData
     vcfs = result.vcfs
     version = result.version
